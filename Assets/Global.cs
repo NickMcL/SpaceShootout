580e22c236bd39b0a098fb0f46ec5b9efac1f5c1
Stali <sstalev@umich.edu>
2016-03-27 19:04:10 -0400

    my stuf

---
+++ Assets/Global.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour {
+    public static Global S;
+	// Use this for initialization
+	void Awake () {
+        S = this;
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public int PlayerSelecting = 1;
+
+    public void HoverOverCharacter()
+    {
+        print("HOVER");
+    }
+
+    public void ChooseCharacter()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
