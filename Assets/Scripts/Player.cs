911298b2bffa457d14f3a615c5e9788734fcc66b
alanjara <alanjara@umich.edu>
2016-03-27 22:03:40 -0400

    preparation for dribble and shoot

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,33 +7,41 @@ public class Player : MonoBehaviour {
         public string skate, Shoot, special;
     };
     controls my_inputs;
-   public int my_number = 1;
+    public int my_number = 1;
     Rigidbody2D rigid;
-     float max_speed = 5;
-     float acceleration = 30;
-     float dash_delay_time = 0.3f;
-     float dash_delay= 0;
-     float slow_delay_time = 1f;
-     float slow_delay= 0;
-     int slowed = 0;
-     float fatigue = 0;
-     bool dash = false;
-     void Start() {
+    float max_speed = 5;
+    float acceleration = 30;
+    float dash_delay_time = 0.3f;
+    float dash_delay = 0;
+    float slow_delay_time = 1f;
+    float slow_delay = 0;
+    int slowed = 0;
+    float fatigue = 0;
+    bool dash = false;
+    public bool has_ball = false;
+    public bool is_goalie = false;
+    GameObject ball;
+    Rigidbody2D ball_rb;
+
+    void Start() {
+        ball = SoccerBall.Ball;
+        ball_rb = ball.GetComponent<Rigidbody2D>();
         my_inputs.vert = string.Format("Vertical{0}", my_number);
         my_inputs.hor = string.Format("Horizontal{0}", my_number);
         my_inputs.skate = string.Format("Skate{0}", my_number);
         my_inputs.Shoot = string.Format("Shoot{0}", my_number);
         my_inputs.special = string.Format("Special{0}", my_number);
         rigid = gameObject.GetComponent<Rigidbody2D>();
-
     }
 
     void Update() {
         checkDash();
         updateMovement();
-       
+        if (has_ball == true) {
+            dribble();
+        }
         if (getInputFire()) {
-            
+            shoot();
         }
 
     }
@@ -50,8 +58,8 @@ public class Player : MonoBehaviour {
             rigid.velocity = rigid.velocity/(1+slowed*0.2f);
         }
           */
-        rigid.AddForce(move_vector/(1+slowed*0.75f));
-        if (rigid.velocity.magnitude > max_speed && dash_delay<dash_delay_time*0.8f) {
+        rigid.AddForce(move_vector / (1 + slowed * 0.75f));
+        if (rigid.velocity.magnitude > max_speed && dash_delay < dash_delay_time * 0.8f) {
             rigid.velocity *= 0.97f;
         }
 
@@ -74,7 +82,7 @@ public class Player : MonoBehaviour {
     void checkDash() {
         if (slow_delay > 0) {
             slow_delay -= Time.deltaTime;
-        } else if(slowed>0){
+        } else if (slowed > 0) {
             slowed--;
             if (slowed > 0) {
                 slow_delay = slow_delay_time;
@@ -84,7 +92,7 @@ public class Player : MonoBehaviour {
         if (Input.GetAxis(my_inputs.skate) > 0 && dash_delay <= 0 && slowed < 3) {
             dash = true;
             dash_delay = dash_delay_time;
-        } else if (dash_delay>0) {
+        } else if (dash_delay > 0) {
             dash_delay -= Time.deltaTime;
             if (dash_delay <= 0) {
                 slowed++;
@@ -92,8 +100,20 @@ public class Player : MonoBehaviour {
             }
         }
     }
+    void dribble() {
+        Rigidbody2D ball_rb = ball.GetComponent<Rigidbody2D>();
 
+
+
+    }
+    void shoot() {
+        Vector2 shot = ball.transform.position-transform.position;
+        Vector3.Normalize(shot);
+        shot *= 10f;
+        ball_rb.AddForce(shot);
+    }
     bool getInputPower() {
         return Input.GetAxis(my_inputs.special) > 0;
     }
+
 }
--- Assets/Scripts/SoccerBall.cs
+++ Assets/Scripts/SoccerBall.cs
@@ -1,30 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SoccerBallAnimator : MonoBehaviour {
+public class SoccerBall : MonoBehaviour {
     Rigidbody2D rb;
     Rigidbody ballrb;
     float max_speed = 50;
-	// Use this for initialization
-	void Start () {
+    public static GameObject Ball;
+    // Use this for initialization
+    void Awake() {
+        Ball = gameObject;
+    }
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         ballrb = transform.GetChild(0).gameObject.GetComponent<Rigidbody>();
-	}
+    }
 
     Vector3 WayToGo;
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         WayToGo.x = rb.velocity.y;
-        WayToGo.y =- rb.velocity.x;
+        WayToGo.y = -rb.velocity.x;
         ballrb.angularVelocity = WayToGo;
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
         while (rb.velocity.magnitude > max_speed) {
             rb.velocity *= 0.99f;
         }
-            
-	}
+
+    }
 }
