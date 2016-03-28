fd90529f7358ad95b14d3ae2d4d6bd9a0ffd1d69
FriedRice <n.mclaughlin21@gmail.com>
2016-03-27 20:38:10 -0400

    Added asteroids./

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -5,11 +5,19 @@ using System.Collections.Generic;
 public class Util : MonoBehaviour{
 
     public static Vector2 lerp(GameObject[] points, float u, int i0 = 0, int i1 = -1) {
+        Vector2[] vpoints = new Vector2[points.Length];
+        for (int i = 0; i < points.Length; ++i) {
+            vpoints[i] = points[i].transform.position;
+        }
+        return lerp(vpoints, u, i0, i1);
+    }
+
+    public static Vector2 lerp(Vector2[] points, float u, int i0 = 0, int i1 = -1) {
         if (i1 == -1) {
             i1 = points.Length - 1;
         }
         if (i0 == i1) {
-            return points[i0].transform.position;
+            return points[i0];
         }
 
         Vector2 point1 = lerp(points, u, i0, i1 - 1);
