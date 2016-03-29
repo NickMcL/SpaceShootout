75d06735df365a9fc445036bf7494324ea3291d6
Wesley Howell <wjhowell@umich.edu>
2016-03-29 00:32:45 -0400

    started reseting breakable astroids

--- Assets/Scripts/Asteriod.cs
+++ Assets/Scripts/Asteriod.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 public class Asteriod : MonoBehaviour {
     public List<GameObject> asteroid_list;
 
+    public bool doLerp = true;
+    public float launchSpeed;
+
     public float move_speed;
     public float min_lerp_offset;
     public float max_lerp_offset;
@@ -16,24 +19,29 @@ public class Asteriod : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; i < lerp_points.Length; ++i) {
+        for (int i = 0; i < lerp_points.Length; ++i)
+        {
             lerp_points[i] = new Vector3(
                 transform.position.x + UnityEngine.Random.Range(min_lerp_offset, max_lerp_offset),
                 transform.position.y + UnityEngine.Random.Range(min_lerp_offset, max_lerp_offset), 0f);
         }
-        lerp_time = Vector3.Distance(lerp_points[0], lerp_points[1]) / move_speed;
+        if(lerp_points.Length > 1) lerp_time = Vector3.Distance(lerp_points[0], lerp_points[1]) / move_speed;
         lerp_start = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    float u = (Time.time - lerp_start) / lerp_time;
-        if (u > 1f) {
-            u = 0f;
-            lerp_start = Time.time;
-            Array.Reverse(lerp_points);
+        if (doLerp)
+        {
+            float u = (Time.time - lerp_start) / lerp_time;
+            if (u > 1f)
+            {
+                u = 0f;
+                lerp_start = Time.time;
+                Array.Reverse(lerp_points);
+            }
+            this.transform.position = Util.lerp(lerp_points, u);
         }
-        this.transform.position = Util.lerp(lerp_points, u);
 	}
 
     public void Destroy()
@@ -42,6 +50,12 @@ public class Asteriod : MonoBehaviour {
         foreach (GameObject obj in asteroid_list)
         {
             GameObject astroid = Instantiate(obj, UnityEngine.Random.insideUnitCircle  + new Vector2(transform.position.x, transform.position.y), Quaternion.identity) as GameObject;
+            Rigidbody2D rb2d = astroid.AddComponent<Rigidbody2D>();
+
+            rb2d.AddForce(new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)) * launchSpeed, ForceMode2D.Impulse);
+            rb2d.gravityScale = 0f;
+            rb2d.mass = 4f;
+            astroid.GetComponent<Asteriod>().doLerp = false;
         }
         if (asteroid_list.Count != 0)
         {
---
+++ Assets/Scripts/ResetObjects.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResetObjects : MonoBehaviour
+{
+    public static ResetObjects S;
+
+    public GameObject[] ObjectsToReset;
+    public List<Vector3> OriginalPositions;
+
+    // Use this for initialization
+    void Awake()
+    {
+        S = this;
+
+        ObjectsToReset = GameObject.FindGameObjectsWithTag("AsteroidBreakable");
+
+        foreach (GameObject obj in ObjectsToReset)
+        {
+            OriginalPositions.Add(obj.transform.position);
+        }
+    }
+
+    public void Reset()
+    {
+        for(int i = 0; i < ObjectsToReset.Length; i++)
+        {
+            GameObject astroid = Instantiate(ObjectsToReset[i], OriginalPositions[i], Quaternion.identity) as GameObject;
+            //ObjectsToReset[i].transform.position = OriginalPositions[i];
+        }
+
+        GameObject[] AsteroidMinis = GameObject.FindGameObjectsWithTag("AsteroidMini");
+        for (var i = 0; i < AsteroidMinis.Length; i++)
+        {
+            Destroy(AsteroidMinis[i]);
+        }
+    }
+
+}
